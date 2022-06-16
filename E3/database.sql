6acdee93825567d483711142cbbeefe9eef46e2c
bin0o <malbino2002@gmail.com>
2022-06-16 23:31:17 +0100

    started populating
    Co-authored-by: Matilde Tocha <matildetocha@users.noreply.github.com>

--- E3/database.sql
+++ E3/database.sql
@@ -10,66 +10,67 @@ DROP TABLE IF EXISTS instalada_em CASCADE;
 DROP TABLE IF EXISTS prateleira CASCADE;
 DROP TABLE IF EXISTS planograma CASCADE;
 DROP TABLE IF EXISTS retalhista CASCADE;
-DROP TABLE IF EXISTS responsável_por CASCADE;
+DROP TABLE IF EXISTS responsavel_por CASCADE;
 DROP TABLE IF EXISTS evento_reposicao CASCADE;
-DROP SEQUENCE IF EXISTS instalada_em_num_serie_seq CASCADE;
-DROP SEQUENCE IF EXISTS ivm_num_serie_seq CASCADE;
-DROP SEQUENCE IF EXISTS planograma_num_serie_seq CASCADE;
-DROP SEQUENCE IF EXISTS responsável_por_num_serie_seq CASCADE;
 
+---------------------------------------------------
+--- TABLES
+---------------------------------------------------
 
- 
 CREATE TABLE categoria
     (nome VARCHAR(80) NOT NULL,
-     PRIMARY KEY(nome));
+     CONSTRAINT pk_categoria PRIMARY KEY(nome));
 
 CREATE TABLE categoria_simples
     (nome VARCHAR(80) NOT NULL,
-     PRIMARY KEY(nome),
-     FOREIGN KEY(nome) REFERENCES categoria(nome));
+     CONSTRAINT pk_categoria_simples PRIMARY KEY(nome),
+     CONSTRAINT fk_categoria_simples_categoria FOREIGN KEY(nome) REFERENCES categoria(nome));
 
 CREATE TABLE super_categoria
     (nome VARCHAR(80) NOT NULL,
-     PRIMARY KEY(nome),
-     FOREIGN KEY(nome) REFERENCES categoria(nome));
+     CONSTRAINT pk_super_categoria PRIMARY KEY(nome),
+     CONSTRAINT fk_super_categoria_categoria FOREIGN KEY(nome) REFERENCES categoria(nome));
 
 CREATE TABLE tem_outra
     (super_categoria CHAR(20) NOT NULL,
      categoria CHAR(20) NOT NULL,
-     PRIMARY KEY(categoria),
-     FOREIGN KEY(categoria) REFERENCES categoria(nome),
-     FOREIGN KEY(super_categoria) REFERENCES super_categoria(nome));
+     CONSTRAINT chk_tem_outra CHECK (super_categoria != categoria),
+     CONSTRAINT pk_tem_outra PRIMARY KEY(categoria),
+     CONSTRAINT fk_tem_outra_categoria FOREIGN KEY(categoria) REFERENCES categoria(nome),
+     CONSTRAINT fk_tem_outra_super_categoria FOREIGN KEY(super_categoria) REFERENCES super_categoria(nome));
 
 CREATE TABLE produto 
     (ean INT NOT NULL,
-     descr VARCHAR(200) NOT NULL,
      cat VARCHAR(80) NOT NULL,
-     PRIMARY KEY(ean),
-     FOREIGN KEY(cat) REFERENCES categoria(nome));
+     descr VARCHAR(200) NOT NULL,
+     CONSTRAINT pk_produto PRIMARY KEY(ean),
+     CONSTRAINT fk_produto_categoria FOREIGN KEY(cat) REFERENCES categoria(nome));
 
 CREATE TABLE tem_categoria
     (ean INT NOT NULL,
      nome VARCHAR(80) NOT NULL,
-     FOREIGN KEY(ean) REFERENCES produto(ean),
-     FOREIGN KEY(nome) REFERENCES categoria(nome));
+     CONSTRAINT pk_tem_categoria_produto PRIMARY KEY(ean,nome),
+     CONSTRAINT fk_tem_categoria_produto FOREIGN KEY(ean) REFERENCES produto(ean),
+     CONSTRAINT fk_tem_categoria_categoria FOREIGN KEY(nome) REFERENCES categoria(nome));
 
 CREATE TABLE IVM 
     (num_serie SERIAL NOT NULL,
      fabricante VARCHAR(20) NOT NULL,
-     PRIMARY KEY(num_serie,fabricante));
+     CONSTRAINT pk_IVM PRIMARY KEY(num_serie,fabricante));
 
 CREATE TABLE ponto_de_retalho
     (nome VARCHAR(50) NOT NULL,
      distrito VARCHAR(20) NOT NULL,
      concelho VARCHAR(20) NOT NULL,
-     PRIMARY KEY(nome));
+     CONSTRAINT pk_ponto_de_retalho PRIMARY KEY(nome));
 
 CREATE TABLE instalada_em
     (num_serie SERIAL NOT NULL,
      fabricante VARCHAR(20) NOT NULL,
      local_ VARCHAR(50) NOT NULL,
-     PRIMARY KEY(num_serie, fabricante),
-     FOREIGN KEY(local_) REFERENCES ponto_de_retalho(nome));
+     CONSTRAINT pk_instalada_em PRIMARY KEY(num_serie, fabricante),
+     CONSTRAINT fk_instalada_em_IVM FOREIGN KEY(num_serie, fabricante) REFERENCES IVM(num_serie, fabricante),
+     CONSTRAINT fk_instalada_em_ponto_de_retalho FOREIGN KEY(local_) REFERENCES ponto_de_retalho(nome));
 
 CREATE TABLE prateleira
     (nro INT NOT NULL,
@@ -77,9 +78,9 @@ CREATE TABLE prateleira
      fabricante VARCHAR(20) NOT NULL,
      altura INT NOT NULL,
      nome VARCHAR(80) NOT NULL,
-     PRIMARY KEY(nro,num_serie, fabricante),
-     FOREIGN KEY(num_serie, fabricante) REFERENCES IVM(num_serie, fabricante),
-     FOREIGN KEY(nome) REFERENCES categoria(nome));
+     CONSTRAINT pk_prateleira PRIMARY KEY(nro,num_serie, fabricante),
+     CONSTRAINT fk_prateleira_IVM FOREIGN KEY(num_serie, fabricante) REFERENCES IVM(num_serie, fabricante),
+     CONSTRAINT fk_prateleira_categoria FOREIGN KEY(nome) REFERENCES categoria(nome));
 
 CREATE TABLE planograma
     (ean INT NOT NULL,
@@ -89,34 +90,127 @@ CREATE TABLE planograma
      faces INT NOT NULL,
      unidades INT NOT NULL, 
      loc VARCHAR(20) NOT NULL,
-     PRIMARY KEY(ean, nro, num_serie, fabricante),
-     FOREIGN KEY(ean) REFERENCES produto(ean),
-     FOREIGN KEY(nro, num_serie, fabricante) REFERENCES prateleira(nro, num_serie, fabricante));
+     CONSTRAINT pk_planograma PRIMARY KEY(ean, nro, num_serie, fabricante),
+     CONSTRAINT fk_planorama_produto FOREIGN KEY(ean) REFERENCES produto(ean),
+     CONSTRAINT fk_planograma_prateleira FOREIGN KEY(nro, num_serie, fabricante) REFERENCES prateleira(nro, num_serie, fabricante));
 
 CREATE TABLE retalhista
     (tin INT NOT NULL,
      name_ VARCHAR(80) NOT NULL UNIQUE,
-     PRIMARY KEY(tin));
+     CONSTRAINT pk_retalhista PRIMARY KEY(tin));
 
-CREATE TABLE responsável_por    
+CREATE TABLE responsavel_por    
     (nome_cat VARCHAR(80) NOT NULL,
      tin INT NOT NULL,
      num_serie SERIAL NOT NULL,
      fabricante VARCHAR(20) NOT NULL,
-     PRIMARY KEY(num_serie, fabricante),
-     FOREIGN KEY(num_serie, fabricante) REFERENCES IVM(num_serie, fabricante),
-     FOREIGN KEY(tin) REFERENCES retalhista(tin),
-     FOREIGN KEY(nome_cat) REFERENCES categoria(nome));
+     CONSTRAINT pk_responsavel_por PRIMARY KEY(num_serie, fabricante),
+     CONSTRAINT fk_responsavel_for_IVM FOREIGN KEY(num_serie, fabricante) REFERENCES IVM(num_serie, fabricante),
+     CONSTRAINT fk_responsavel_por_retalhista FOREIGN KEY(tin) REFERENCES retalhista(tin),
+     CONSTRAINT fk_responsavel_por_categoria FOREIGN KEY(nome_cat) REFERENCES categoria(nome));
 
 CREATE TABLE evento_reposicao
     (ean INT NOT NULL,
      nro INT NOT NULL,
      num_serie INT NOT NULL,
      fabricante VARCHAR(20) NOT NULL,
-     instante DATE NOT NULL,
+     instante TIMESTAMP NOT NULL,
      unidades INT NOT NULL,
      tin INT NOT NULL,
-     PRIMARY KEY(ean, nro, num_serie, fabricante, instante),
-     FOREIGN KEY(tin) REFERENCES retalhista(tin));
+     CONSTRAINT pk_evento_reposicao PRIMARY KEY(ean, nro, num_serie, fabricante, instante),
+     CONSTRAINT fk_evento_reposicao_planograma FOREIGN KEY(ean, nro, num_serie, fabricante) REFERENCES planograma(ean, nro, num_serie, fabricante),
+     CONSTRAINT fk_evento_reposicao_retalhista FOREIGN KEY(tin) REFERENCES retalhista(tin));
 
 /* manuel is a little bitch. my little bitch :) */
+---------------------------------------------------
+-- POPULATING
+---------------------------------------------------
+-- Categoria
+INSERT INTO categoria VALUES ('Barras'); 
+INSERT INTO categoria VALUES ('Barras Energéticas');
+INSERT INTO categoria VALUES ('Barras de Frutas');
+INSERT INTO categoria VALUES ('Bebidas');
+INSERT INTO categoria VALUES ('Refrigerantes');
+INSERT INTO categoria VALUES ('Bebidas Alcoólicas');
+INSERT INTO categoria VALUES ('Bebidas Energéticas');
+INSERT INTO categoria VALUES ('Sumos de Fruta');
+INSERT INTO categoria VALUES ('Fruta');
+INSERT INTO categoria VALUES ('Legumes');
+INSERT INTO categoria VALUES ('Sopas');
+INSERT INTO categoria VALUES ('Sopa Miso');
+INSERT INTO categoria VALUES ('Sopa de Cenoura');
+INSERT INTO categoria VALUES ('Sopa de Favas');
+
+-- Categoria Simples
+INSERT INTO categoria_simples VALUES ('Barras Energéticas');
+INSERT INTO categoria_simples VALUES ('Barras de Frutas');
+INSERT INTO categoria_simples VALUES ('Refrigerantes');
+INSERT INTO categoria_simples VALUES ('Bebidas Alcoólicas');
+INSERT INTO categoria_simples VALUES ('Bebidas Energéticas');
+INSERT INTO categoria_simples VALUES ('Sumos de Fruta');
+INSERT INTO categoria_simples VALUES ('Fruta');
+INSERT INTO categoria_simples VALUES ('Legumes');
+INSERT INTO categoria_simples VALUES ('Sopa Miso');
+INSERT INTO categoria_simples VALUES ('Sopa de Cenoura');
+INSERT INTO categoria_simples VALUES ('Sopa de Favas');
+
+-- Super Categoria
+INSERT INTO super_categoria VALUES ('Barras'); 
+INSERT INTO super_categoria VALUES ('Bebidas');
+INSERT INTO super_categoria VALUES ('Sopas');
+
+-- Tem Outra
+INSERT INTO tem_outra VALUES ('Barras','Barras Energéticas');
+INSERT INTO tem_outra VALUES ('Barras','Barras de Frutas');
+INSERT INTO tem_outra VALUES ('Bebidas','Refrigerantes');
+INSERT INTO tem_outra VALUES ('Bebidas', 'Bebidas Alcoólicas');
+INSERT INTO tem_outra VALUES ('Bebidas', 'Bebidas Energéticas');
+INSERT INTO tem_outra VALUES ('Bebidas', 'Sumos de Fruta');
+INSERT INTO tem_outra VALUES ('Sopas', 'Sopa Miso');
+INSERT INTO tem_outra VALUES ('Sopas', 'Sopa de Cenoura');
+INSERT INTO tem_outra VALUES ('Sopas', 'Sopa de Favas');
+
+INSERT INTO produto VALUES ('60','Refrigerantes','Sumol');
+INSERT INTO produto VALUES ('70','Barras Energéticas','Barra Prozis');
+INSERT INTO produto VALUES ('80','Fruta','Laranja');
+INSERT INTO produto VALUES ('90','Legumes','Espinafre');
+INSERT INTO produto VALUES ('100','Barras Energéticas','Barra MyProtein');
+INSERT INTO produto VALUES ('110','Sopa Miso','Sopa Miso1');
+INSERT INTO produto VALUES ('120','Bebidas Alcoólicas','Vodka');
+INSERT INTO produto VALUES ('130','Refrigerantes','Coca-Cola');
+
+INSERT INTO tem_categoria VALUES ('60','Refrigerantes');
+INSERT INTO tem_categoria VALUES ('70','Barras Energéticas');
+INSERT INTO tem_categoria VALUES ('80','Fruta');
+INSERT INTO tem_categoria VALUES ('90','Legumes');
+INSERT INTO tem_categoria VALUES ('100','Barras Energéticas');
+INSERT INTO tem_categoria VALUES ('110','Sopa Miso');
+INSERT INTO tem_categoria VALUES ('120','Bebidas Alcoólicas');
+INSERT INTO tem_categoria VALUES ('130','Refrigerantes');
+
+INSERT INTO IVM VALUES ('1','Bosch');
+INSERT INTO IVM VALUES ('2','Rowenta');
+INSERT INTO IVM VALUES ('3','Bosch');
+INSERT INTO IVM VALUES ('4','Atlante');
+INSERT INTO IVM VALUES ('5','Cristallo');
+
+INSERT INTO ponto_de_retalho VALUES ('IST-Taguspark','Lisboa','Oeiras');
+INSERT INTO ponto_de_retalho VALUES ('Repsol-Lisboa','Lisboa','Oriente');
+INSERT INTO ponto_de_retalho VALUES ('Fórum-Castelo Branco','Castelo Branco','Castelo Branco');
+
+INSERT INTO instalada_em VALUES ('1','Bosch','Repsol-Lisboa');
+INSERT INTO instalada_em VALUES ('2','Rowenta','Fórum-Castelo Branco');
+INSERT INTO instalada_em VALUES ('3','Bosch','IST-Taguspark');
+INSERT INTO instalada_em VALUES ('4','Atlante','IST-Taguspark');
+INSERT INTO instalada_em VALUES ('5','Cristallo','Fórum-Castelo Branco');
+
+--INSERT INTO prateleira VALUES ('1','1','Bosch','15','48');
+--INSERT INTO prateleira VALUES ('2','1','Bosch','15','48');
+--INSERT INTO prateleira VALUES ()
+
+
+
+
+
+
+  
