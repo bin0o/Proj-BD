08be57f9e984affe85875643063a6c886da73c54
bin0o <malbino2002@gmail.com>
2022-06-18 20:43:12 +0100

    database a funcionar, falta acrescentar. Amo-te muito <33333333333333333333333333
    Co-authored-by: Matilde Tocha <matildetocha@users.noreply.github.com>

--- E3/database.sql
+++ E3/database.sql
@@ -40,14 +40,14 @@ CREATE TABLE tem_outra
      CONSTRAINT fk_tem_outra_super_categoria FOREIGN KEY(super_categoria) REFERENCES super_categoria(nome));
 
 CREATE TABLE produto 
-    (ean INT NOT NULL,
+    (ean SERIAL NOT NULL,
      cat VARCHAR(80) NOT NULL,
      descr VARCHAR(200) NOT NULL,
      CONSTRAINT pk_produto PRIMARY KEY(ean),
      CONSTRAINT fk_produto_categoria FOREIGN KEY(cat) REFERENCES categoria(nome));
 
 CREATE TABLE tem_categoria
-    (ean INT NOT NULL,
+    (ean SERIAL NOT NULL,
      nome VARCHAR(80) NOT NULL,
      CONSTRAINT pk_tem_categoria_produto PRIMARY KEY(ean,nome),
      CONSTRAINT fk_tem_categoria_produto FOREIGN KEY(ean) REFERENCES produto(ean),
@@ -74,7 +74,7 @@ CREATE TABLE instalada_em
 
 CREATE TABLE prateleira
     (nro INT NOT NULL,
-     num_serie INT NOT NULL,
+     num_serie SERIAL NOT NULL,
      fabricante VARCHAR(20) NOT NULL,
      altura INT NOT NULL,
      nome VARCHAR(80) NOT NULL,
@@ -83,7 +83,7 @@ CREATE TABLE prateleira
      CONSTRAINT fk_prateleira_categoria FOREIGN KEY(nome) REFERENCES categoria(nome));
 
 CREATE TABLE planograma
-    (ean INT NOT NULL,
+    (ean SERIAL NOT NULL,
      nro INT NOT NULL,
      num_serie SERIAL NOT NULL,
      fabricante VARCHAR(20) NOT NULL,
@@ -110,9 +110,9 @@ CREATE TABLE responsavel_por
      CONSTRAINT fk_responsavel_por_categoria FOREIGN KEY(nome_cat) REFERENCES categoria(nome));
 
 CREATE TABLE evento_reposicao
-    (ean INT NOT NULL,
+    (ean SERIAL NOT NULL,
      nro INT NOT NULL,
-     num_serie INT NOT NULL,
+     num_serie SERIAL NOT NULL,
      fabricante VARCHAR(20) NOT NULL,
      instante TIMESTAMP NOT NULL,
      unidades INT NOT NULL,
@@ -178,6 +178,7 @@ INSERT INTO produto VALUES ('100','Barras Energéticas','Barra MyProtein');
 INSERT INTO produto VALUES ('110','Sopa Miso','Sopa Miso1');
 INSERT INTO produto VALUES ('120','Bebidas Alcoólicas','Vodka');
 INSERT INTO produto VALUES ('130','Refrigerantes','Coca-Cola');
+INSERT INTO produto VALUES ('140','Sumos de Fruta','Sumo de Laranja');
 
 INSERT INTO tem_categoria VALUES ('60','Refrigerantes');
 INSERT INTO tem_categoria VALUES ('70','Barras Energéticas');
@@ -204,9 +205,41 @@ INSERT INTO instalada_em VALUES ('3','Bosch','IST-Taguspark');
 INSERT INTO instalada_em VALUES ('4','Atlante','IST-Taguspark');
 INSERT INTO instalada_em VALUES ('5','Cristallo','Fórum-Castelo Branco');
 
---INSERT INTO prateleira VALUES ('1','1','Bosch','15','48');
---INSERT INTO prateleira VALUES ('2','1','Bosch','15','48');
---INSERT INTO prateleira VALUES ()
+
+INSERT INTO prateleira VALUES ('1','1','Bosch','15','Refrigerantes');
+INSERT INTO prateleira VALUES ('2','3','Bosch','15','Barras Energéticas');
+INSERT INTO prateleira VALUES ('1','4','Atlante','15','Sopas');
+INSERT INTO prateleira VALUES ('2','4','Atlante','15','Fruta');
+INSERT INTO prateleira VALUES ('3','5','Cristallo','15','Fruta');
+INSERT INTO prateleira VALUES ('6','5','Cristallo','15','Bebidas Alcoólicas');
+INSERT INTO prateleira VALUES ('5','2','Rowenta','15','Sumos de Fruta');
+
+
+INSERT INTO planograma(ean, nro, num_serie, fabricante, faces, unidades, loc) VALUES ('60','1','1', 'Bosch','6','48','3');
+INSERT INTO planograma VALUES ('70','2','3', 'Bosch','6','48','5');
+INSERT INTO planograma VALUES ('120','3','5', 'Cristallo','5','40','5');
+INSERT INTO planograma VALUES ('80','2','4', 'Atlante','8','64','8');
+INSERT INTO planograma VALUES ('80','6','5', 'Cristallo','5','40','2');
+INSERT INTO planograma VALUES ('140','5','2', 'Rowenta','4','32','3');
+
+
+INSERT INTO retalhista VALUES('102415639','Auchan');
+INSERT INTO retalhista VALUES('968746229','Recheio');
+INSERT INTO retalhista VALUES('208913249','Lidl');
+INSERT INTO retalhista VALUES('496320710','Intermarché');
+INSERT INTO retalhista VALUES('968720710','Pingo Doce');
+INSERT INTO retalhista VALUES('496326229','Jumbo');
+
+
+INSERT INTO responsavel_por(nome_cat, tin, num_serie, fabricante) VALUES ('Refrigerantes','102415639','1','Bosch');
+INSERT INTO responsavel_por VALUES ('Barras Energéticas','968720710','3','Bosch');
+INSERT INTO responsavel_por VALUES ('Sumos de Fruta','968746229','2','Rowenta');
+INSERT INTO responsavel_por VALUES ('Fruta','208913249','5','Cristallo');
+INSERT INTO responsavel_por VALUES ('Sopas','496320710','4','Atlante');
+
+
+INSERT INTO evento_reposicao(ean, nro, num_serie, fabricante, instante, unidades, tin) VALUES ('60','1', '1','Bosch','18/02/2022','40','102415639');
+
 
 
 
