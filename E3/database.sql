183c626e46edf072017994627f4402a3d05f34f7
matildetocha <matilde.tocha@tecnico.ulisboa.pt>
2022-06-19 14:20:53 +0100

    trying triggers
    Co-authored-by: Manuel Albino <bin0o@users.noreply.github.com>

--- E3/database.sql
+++ E3/database.sql
@@ -126,124 +126,174 @@ CREATE TABLE evento_reposicao
 -- POPULATING
 ---------------------------------------------------
 -- Categoria
-INSERT INTO categoria VALUES ('Barras'); 
-INSERT INTO categoria VALUES ('Barras Energéticas');
-INSERT INTO categoria VALUES ('Barras de Frutas');
-INSERT INTO categoria VALUES ('Bebidas');
-INSERT INTO categoria VALUES ('Refrigerantes');
-INSERT INTO categoria VALUES ('Bebidas Alcoólicas');
-INSERT INTO categoria VALUES ('Bebidas Energéticas');
-INSERT INTO categoria VALUES ('Sumos de Fruta');
-INSERT INTO categoria VALUES ('Fruta');
-INSERT INTO categoria VALUES ('Legumes');
-INSERT INTO categoria VALUES ('Sopas');
-INSERT INTO categoria VALUES ('Sopa Miso');
-INSERT INTO categoria VALUES ('Sopa de Cenoura');
-INSERT INTO categoria VALUES ('Sopa de Favas');
+INSERT INTO categoria VALUES 
+                            ('Barras'),
+                            ('Barras Energéticas'),
+                            ('Barras de Frutas'),
+                            ('Bebidas'),
+                            ('Refrigerantes'),
+                            ('Bebidas Alcoólicas'),
+                            ('Bebidas Energéticas'),
+                            ('Sumos de Fruta'),
+                            ('Fruta'),
+                            ('Legumes'),
+                            ('Sopas'),
+                            ('Sopa Miso'),
+                            ('Sopa de Cenoura'),
+                            ('Sopa de Favas');
 
 -- Categoria Simples
-INSERT INTO categoria_simples VALUES ('Barras Energéticas');
-INSERT INTO categoria_simples VALUES ('Barras de Frutas');
-INSERT INTO categoria_simples VALUES ('Refrigerantes');
-INSERT INTO categoria_simples VALUES ('Bebidas Alcoólicas');
-INSERT INTO categoria_simples VALUES ('Bebidas Energéticas');
-INSERT INTO categoria_simples VALUES ('Sumos de Fruta');
-INSERT INTO categoria_simples VALUES ('Fruta');
-INSERT INTO categoria_simples VALUES ('Legumes');
-INSERT INTO categoria_simples VALUES ('Sopa Miso');
-INSERT INTO categoria_simples VALUES ('Sopa de Cenoura');
-INSERT INTO categoria_simples VALUES ('Sopa de Favas');
+INSERT INTO categoria_simples VALUES 
+                                    ('Barras Energéticas'),
+                                    ('Barras de Frutas'),
+                                    ('Refrigerantes'),
+                                    ('Bebidas Alcoólicas'),
+                                    ('Bebidas Energéticas'),
+                                    ('Sumos de Fruta'),
+                                    ('Fruta'),
+                                    ('Legumes'),
+                                    ('Sopa Miso'),
+                                    ('Sopa de Cenoura'),
+                                    ('Sopa de Favas');
 
 -- Super Categoria
-INSERT INTO super_categoria VALUES ('Barras'); 
-INSERT INTO super_categoria VALUES ('Bebidas');
-INSERT INTO super_categoria VALUES ('Sopas');
+INSERT INTO super_categoria VALUES 
+                                    ('Barras'),
+                                    ('Bebidas'),
+                                    ('Sopas');
 
 -- Tem Outra
-INSERT INTO tem_outra VALUES ('Barras','Barras Energéticas');
-INSERT INTO tem_outra VALUES ('Barras','Barras de Frutas');
-INSERT INTO tem_outra VALUES ('Bebidas','Refrigerantes');
-INSERT INTO tem_outra VALUES ('Bebidas', 'Bebidas Alcoólicas');
-INSERT INTO tem_outra VALUES ('Bebidas', 'Bebidas Energéticas');
-INSERT INTO tem_outra VALUES ('Bebidas', 'Sumos de Fruta');
-INSERT INTO tem_outra VALUES ('Sopas', 'Sopa Miso');
-INSERT INTO tem_outra VALUES ('Sopas', 'Sopa de Cenoura');
-INSERT INTO tem_outra VALUES ('Sopas', 'Sopa de Favas');
+INSERT INTO tem_outra VALUES 
+                            ('Barras','Barras Energéticas'),
+                            ('Barras','Barras de Frutas'),
+                            ('Bebidas','Refrigerantes'),
+                            ('Bebidas', 'Bebidas Alcoólicas'),
+                            ('Bebidas', 'Bebidas Energéticas'),
+                            ('Bebidas', 'Sumos de Fruta'),
+                            ('Sopas', 'Sopa Miso'),
+                            ('Sopas', 'Sopa de Cenoura'),
+                            ('Sopas', 'Sopa de Favas');
 
-INSERT INTO produto VALUES ('60','Refrigerantes','Sumol');
-INSERT INTO produto VALUES ('70','Barras Energéticas','Barra Prozis');
-INSERT INTO produto VALUES ('80','Fruta','Laranja');
-INSERT INTO produto VALUES ('90','Legumes','Espinafre');
-INSERT INTO produto VALUES ('100','Barras Energéticas','Barra MyProtein');
-INSERT INTO produto VALUES ('110','Sopa Miso','Sopa Miso1');
-INSERT INTO produto VALUES ('120','Bebidas Alcoólicas','Vodka');
-INSERT INTO produto VALUES ('130','Refrigerantes','Coca-Cola');
-INSERT INTO produto VALUES ('140','Sumos de Fruta','Sumo de Laranja');
+-- Produto
+INSERT INTO produto VALUES 
+                        ('10','Barras Energéticas','Barra PB&J Prozis'),
+                        ('20','Barras Energéticas','Barra Chocolate MyProtein'),
+                        ('30','Barras de Frutas','Barra Maçã e Canela'),
+                        ('40','Refrigerantes','Sumol'),
+                        ('50','Refrigerantes','Coca-Cola'),
+                        ('60','Bebidas Alcoólicas','Vodka'),
+                        ('70','Bebidas Energéticas','Red Bull'),
+                        ('80','Bebidas Energéticas','Monster'),
+                        ('90','Sumos de Fruta','Sumo de Manga-Laranja'),
+                        ('100','Sumos de Fruta','Sumo de Maçã'),
+                        ('110','Fruta','Laranja'),
+                        ('120','Fruta','Pêra'),
+                        ('130','Legumes','Espinafre'),
+                        ('140','Legumes','Cenoura'),
+                        ('150','Sopa Miso','Sopa Miso Simples'),
+                        ('160','Sopa Miso','Sopa Miso com Cogumelos');
 
-INSERT INTO tem_categoria VALUES ('60','Refrigerantes');
-INSERT INTO tem_categoria VALUES ('70','Barras Energéticas');
-INSERT INTO tem_categoria VALUES ('80','Fruta');
-INSERT INTO tem_categoria VALUES ('90','Legumes');
-INSERT INTO tem_categoria VALUES ('100','Barras Energéticas');
-INSERT INTO tem_categoria VALUES ('110','Sopa Miso');
-INSERT INTO tem_categoria VALUES ('120','Bebidas Alcoólicas');
-INSERT INTO tem_categoria VALUES ('130','Refrigerantes');
+-- Tem Categoria
+INSERT INTO tem_categoria VALUES
+                                ('10','Barras Energéticas'),
+                                ('20','Barras Energéticas'),
+                                ('30','Barras de Frutas'),
+                                ('40','Refrigerantes'),
+                                ('50','Refrigerantes'),
+                                ('60', 'Bebidas Alcoólicas'),
+                                ('70','Barras Energéticas'),
+                                ('80','Bebidas Energéticas'),
+                                ('90','Sumos de Fruta'),
+                                ('100','Sumos de Fruta'),
+                                ('110','Fruta'),
+                                ('120','Fruta'),
+                                ('130','Legumes'),
+                                ('140','Legumes'),
+                                ('150','Sopa Miso'),
+                                ('160','Sopa Miso');
 
-INSERT INTO IVM VALUES ('1','Bosch');
-INSERT INTO IVM VALUES ('2','Rowenta');
-INSERT INTO IVM VALUES ('3','Bosch');
-INSERT INTO IVM VALUES ('4','Atlante');
-INSERT INTO IVM VALUES ('5','Cristallo');
+-- IVM
+INSERT INTO IVM VALUES 
+                    ('1','Bosch'),
+                    ('2','Rowenta'),
+                    ('3','Bosch'),
+                    ('4','Atlante'),
+                    ('5','Cristallo');
 
-INSERT INTO ponto_de_retalho VALUES ('IST-Taguspark','Lisboa','Oeiras');
-INSERT INTO ponto_de_retalho VALUES ('Repsol-Lisboa','Lisboa','Oriente');
-INSERT INTO ponto_de_retalho VALUES ('Fórum-Castelo Branco','Castelo Branco','Castelo Branco');
+-- Ponto de Retalho
+INSERT INTO ponto_de_retalho VALUES 
+                                    ('IST-Taguspark','Lisboa','Oeiras'),
+                                    ('Repsol-Lisboa','Lisboa','Oriente'),
+                                    ('Fórum-Castelo Branco','Castelo Branco','Castelo Branco');
 
-INSERT INTO instalada_em VALUES ('1','Bosch','Repsol-Lisboa');
-INSERT INTO instalada_em VALUES ('2','Rowenta','Fórum-Castelo Branco');
-INSERT INTO instalada_em VALUES ('3','Bosch','IST-Taguspark');
-INSERT INTO instalada_em VALUES ('4','Atlante','IST-Taguspark');
-INSERT INTO instalada_em VALUES ('5','Cristallo','Fórum-Castelo Branco');
+-- Instalada Em
+INSERT INTO instalada_em VALUES 
+                                ('1','Bosch','Repsol-Lisboa'),
+                                ('2','Rowenta','Fórum-Castelo Branco'),
+                                ('3','Bosch','IST-Taguspark'),
+                                ('4','Atlante','IST-Taguspark'),
+                                ('5','Cristallo','Fórum-Castelo Branco');
+
+-- Prateleira
+INSERT INTO prateleira VALUES 
+                                ('1','1','Bosch','15','Refrigerantes'),
+                                ('2','3','Bosch','15','Barras Energéticas'),
+                                ('1','4','Atlante','15','Sopas'),
+                                ('2','4','Atlante','15','Fruta'),
+                                ('3','5','Cristallo','15','Fruta'),
+                                ('6','5','Cristallo','15','Bebidas Alcoólicas'),
+                                ('5','2','Rowenta','15','Sumos de Fruta');
+
+-- Planograma
+INSERT INTO planograma VALUES 
+                                ('60','1','1', 'Bosch','6','48','3'),
+                                ('70','2','3', 'Bosch','6','48','5'),
+                                ('120','3','5', 'Cristallo','5','40','5'),
+                                ('80','2','4', 'Atlante','8','64','8'),
+                                ('80','6','5', 'Cristallo','5','40','2'),
+                                ('140','5','2', 'Rowenta','4','32','3');
+
+-- Retalhista
+INSERT INTO retalhista VALUES
+                                ('102415639','Auchan'),
+                                ('968746229','Recheio'),
+                                ('208913249','Lidl'),
+                                ('496320710','Intermarché'),
+                                ('968720710','Pingo Doce'),
+                                ('496326229','Jumbo');
+
+-- Responsável Por
+INSERT INTO responsavel_por VALUES 
+                                    ('Refrigerantes','102415639','1','Bosch'),
+                                    ('Barras Energéticas','968720710','3','Bosch'),
+                                    ('Sumos de Fruta','968746229','2','Rowenta'),
+                                    ('Fruta','208913249','5','Cristallo'),
+                                    ('Sopas','496320710','4','Atlante');
+
+-- Evento Reposição
+INSERT INTO evento_reposicao VALUES ('60','1', '1','Bosch','18/02/2022','40','102415639'),
+                                    ('120','3','5','Cristallo','21/05/2002','45','208913249');
 
 
-INSERT INTO prateleira VALUES ('1','1','Bosch','15','Refrigerantes');
-INSERT INTO prateleira VALUES ('2','3','Bosch','15','Barras Energéticas');
-INSERT INTO prateleira VALUES ('1','4','Atlante','15','Sopas');
-INSERT INTO prateleira VALUES ('2','4','Atlante','15','Fruta');
-INSERT INTO prateleira VALUES ('3','5','Cristallo','15','Fruta');
-INSERT INTO prateleira VALUES ('6','5','Cristallo','15','Bebidas Alcoólicas');
-INSERT INTO prateleira VALUES ('5','2','Rowenta','15','Sumos de Fruta');
-
-
-INSERT INTO planograma(ean, nro, num_serie, fabricante, faces, unidades, loc) VALUES ('60','1','1', 'Bosch','6','48','3');
-INSERT INTO planograma VALUES ('70','2','3', 'Bosch','6','48','5');
-INSERT INTO planograma VALUES ('120','3','5', 'Cristallo','5','40','5');
-INSERT INTO planograma VALUES ('80','2','4', 'Atlante','8','64','8');
-INSERT INTO planograma VALUES ('80','6','5', 'Cristallo','5','40','2');
-INSERT INTO planograma VALUES ('140','5','2', 'Rowenta','4','32','3');
-
-
-INSERT INTO retalhista VALUES('102415639','Auchan');
-INSERT INTO retalhista VALUES('968746229','Recheio');
-INSERT INTO retalhista VALUES('208913249','Lidl');
-INSERT INTO retalhista VALUES('496320710','Intermarché');
-INSERT INTO retalhista VALUES('968720710','Pingo Doce');
-INSERT INTO retalhista VALUES('496326229','Jumbo');
-
-
-INSERT INTO responsavel_por(nome_cat, tin, num_serie, fabricante) VALUES ('Refrigerantes','102415639','1','Bosch');
-INSERT INTO responsavel_por VALUES ('Barras Energéticas','968720710','3','Bosch');
-INSERT INTO responsavel_por VALUES ('Sumos de Fruta','968746229','2','Rowenta');
-INSERT INTO responsavel_por VALUES ('Fruta','208913249','5','Cristallo');
-INSERT INTO responsavel_por VALUES ('Sopas','496320710','4','Atlante');
-
-
-INSERT INTO evento_reposicao(ean, nro, num_serie, fabricante, instante, unidades, tin) VALUES ('60','1', '1','Bosch','18/02/2022','40','102415639');
 
 
 
+---------------------------------------------------
+-- CONSTRAINTS
+---------------------------------------------------
+--Triggers
 
+CREATE FUNCTION chk_unidades_reposicao_proc()
+RETURN VOID AS
+$$
+BEGIN
+    IF NEW.unidades > planograma.unidades THEN
+        RAISE EXCEPTION 'Unidades repostas não podem exceder as do planograma'
+    END IF;
+END;
+$$ LANGUAGE sql;
 
-
-
+CREATE TRIGGER chk_unidades_reposicao_trigger
+BEFORE INSERT ON evento_reposicao
+FOR EACH ROW EXECUTE PROCEDURE chk_unidades_reposicao_proc();
   
